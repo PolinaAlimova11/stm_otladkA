42c21a46dcdfc42550934aad23f81f41d7823b91
PolinaAlimova11 <56290572+PolinaAlimova11@users.noreply.github.com>
2022-04-06 12:28:22 +0300

    Update main.c

--- main.c
+++ main.c
@@ -104,15 +104,76 @@ void setnumber (uint8_t number)
 			
 		case 4:
 			{
-				HAL_GPIO_WritePin(Led_A_GPIO_Port, Led_A_Pin, GPIO_PIN_SET); //A
+				HAL_GPIO_WritePin(Led_A_GPIO_Port, Led_A_Pin, GPIO_PIN_RESET); //A
 				HAL_GPIO_WritePin(Led_B_GPIO_Port, Led_B_Pin, GPIO_PIN_SET); //B
-				HAL_GPIO_WritePin(Led_C_GPIO_Port, Led_C_Pin, GPIO_PIN_RESET); //C
-				HAL_GPIO_WritePin(Led_D_GPIO_Port, Led_D_Pin, GPIO_PIN_SET); //D
-				HAL_GPIO_WritePin(Led_E_GPIO_Port, Led_E_Pin, GPIO_PIN_SET); //E
-				HAL_GPIO_WritePin(Led_F_GPIO_Port, Led_F_Pin, GPIO_PIN_RESET);	//F
+				HAL_GPIO_WritePin(Led_C_GPIO_Port, Led_C_Pin, GPIO_PIN_SET); //C
+				HAL_GPIO_WritePin(Led_D_GPIO_Port, Led_D_Pin, GPIO_PIN_RESET); //D
+				HAL_GPIO_WritePin(Led_E_GPIO_Port, Led_E_Pin, GPIO_PIN_RESET); //E
+				HAL_GPIO_WritePin(Led_F_GPIO_Port, Led_F_Pin, GPIO_PIN_SET);	//F
 				HAL_GPIO_WritePin(Led_G_GPIO_Port, Led_G_Pin, GPIO_PIN_SET);  //G
 				break;
 			}
+
+		case 5:
+			{
+				HAL_GPIO_WritePin(Led_A_GPIO_Port, Led_A_Pin, GPIO_PIN_SET); //A
+				HAL_GPIO_WritePin(Led_B_GPIO_Port, Led_B_Pin, GPIO_PIN_RESET); //B
+				HAL_GPIO_WritePin(Led_C_GPIO_Port, Led_C_Pin, GPIO_PIN_SET); //C
+				HAL_GPIO_WritePin(Led_D_GPIO_Port, Led_D_Pin, GPIO_PIN_SET); //D
+				HAL_GPIO_WritePin(Led_E_GPIO_Port, Led_E_Pin, GPIO_PIN_RESET); //E
+				HAL_GPIO_WritePin(Led_F_GPIO_Port, Led_F_Pin, GPIO_PIN_SET);	//F
+				HAL_GPIO_WritePin(Led_G_GPIO_Port, Led_G_Pin, GPIO_PIN_SET);  //G
+				break;
+			}
+
+		case 6:
+			{
+				HAL_GPIO_WritePin(Led_A_GPIO_Port, Led_A_Pin, GPIO_PIN_SET); //A
+				HAL_GPIO_WritePin(Led_B_GPIO_Port, Led_B_Pin, GPIO_PIN_RESET); //B
+				HAL_GPIO_WritePin(Led_C_GPIO_Port, Led_C_Pin, GPIO_PIN_SET); //C
+				HAL_GPIO_WritePin(Led_D_GPIO_Port, Led_D_Pin, GPIO_PIN_SET); //D
+				HAL_GPIO_WritePin(Led_E_GPIO_Port, Led_E_Pin, GPIO_PIN_SET); //E
+				HAL_GPIO_WritePin(Led_F_GPIO_Port, Led_F_Pin, GPIO_PIN_SET);	//F
+				HAL_GPIO_WritePin(Led_G_GPIO_Port, Led_G_Pin, GPIO_PIN_SET);  //G
+				break;
+			}
+
+		case 7:
+			{
+				HAL_GPIO_WritePin(Led_A_GPIO_Port, Led_A_Pin, GPIO_PIN_SET); //A
+				HAL_GPIO_WritePin(Led_B_GPIO_Port, Led_B_Pin, GPIO_PIN_SET); //B
+				HAL_GPIO_WritePin(Led_C_GPIO_Port, Led_C_Pin, GPIO_PIN_SET); //C
+				HAL_GPIO_WritePin(Led_D_GPIO_Port, Led_D_Pin, GPIO_PIN_RESET); //D
+				HAL_GPIO_WritePin(Led_E_GPIO_Port, Led_E_Pin, GPIO_PIN_RESET); //E
+				HAL_GPIO_WritePin(Led_F_GPIO_Port, Led_F_Pin, GPIO_PIN_RESET);	//F
+				HAL_GPIO_WritePin(Led_G_GPIO_Port, Led_G_Pin, GPIO_PIN_RESET);  //G
+				break;
+			}
+
+		case 8:
+			{
+				HAL_GPIO_WritePin(Led_A_GPIO_Port, Led_A_Pin, GPIO_PIN_SET); //A
+				HAL_GPIO_WritePin(Led_B_GPIO_Port, Led_B_Pin, GPIO_PIN_SET); //B
+				HAL_GPIO_WritePin(Led_C_GPIO_Port, Led_C_Pin, GPIO_PIN_SET); //C
+				HAL_GPIO_WritePin(Led_D_GPIO_Port, Led_D_Pin, GPIO_PIN_SET); //D
+				HAL_GPIO_WritePin(Led_E_GPIO_Port, Led_E_Pin, GPIO_PIN_SET); //E
+				HAL_GPIO_WritePin(Led_F_GPIO_Port, Led_F_Pin, GPIO_PIN_SET);	//F
+				HAL_GPIO_WritePin(Led_G_GPIO_Port, Led_G_Pin, GPIO_PIN_SET);  //G
+				break;
+			}
+
+		case 9:
+			{
+				HAL_GPIO_WritePin(Led_A_GPIO_Port, Led_A_Pin, GPIO_PIN_SET); //A
+				HAL_GPIO_WritePin(Led_B_GPIO_Port, Led_B_Pin, GPIO_PIN_SET); //B
+				HAL_GPIO_WritePin(Led_C_GPIO_Port, Led_C_Pin, GPIO_PIN_SET); //C
+				HAL_GPIO_WritePin(Led_D_GPIO_Port, Led_D_Pin, GPIO_PIN_SET); //D
+				HAL_GPIO_WritePin(Led_E_GPIO_Port, Led_E_Pin, GPIO_PIN_RESET); //E
+				HAL_GPIO_WritePin(Led_F_GPIO_Port, Led_F_Pin, GPIO_PIN_SET);	//F
+				HAL_GPIO_WritePin(Led_G_GPIO_Port, Led_G_Pin, GPIO_PIN_SET);  //G
+				break;
+			}
+
 		
 
 	}
@@ -192,12 +253,10 @@ int main(void)
 	  			HAL_GPIO_WritePin(Led_E_GPIO_Port, Led_E_Pin, GPIO_PIN_RESET); //E
 	  			HAL_GPIO_WritePin(Led_F_GPIO_Port, Led_F_Pin, GPIO_PIN_RESET);	//F
 	  			HAL_GPIO_WritePin(Led_G_GPIO_Port, Led_G_Pin, GPIO_PIN_RESET);  //G*/
-	  HAL_Delay(1000);
-	  setnumber(number);
-	  HAL_Delay(1000);
-	  number++;
-	  setnumber(number);
-	  number--;
+	 for (int i=9; i>=0; i--){
+		  setnumber(i);
+		  HAL_Delay(1000);
+	  }
   }
   /* USER CODE END 3 */
 }
